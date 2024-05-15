f9666eb7c69e9e1b1bded1ac77ac80e003c2c849
Mostafa Mahmoud <mostafamahmoud24@gmail.com>
2024-05-15 13:56:49 +0300

    fixed some bugs with the carousel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,21 +4,21 @@ import Hero from "../components/hero/Hero";
 import ItemsCarousel from "../components/itemsCarousel/ItemsCarousel";
 
 function Home() {
-  // const items = [
-  //   <div className="eshta">Item 1</div>,
-  //   <div className="eshta">Item 2</div>,
-  //   <div className="eshta">Item 3</div>,
-  //   <div className="eshta">Item 4</div>,
-  //   <div className="eshta">Item 5</div>,
-  //   <div className="eshta">Item 6</div>,
-  //   <div className="eshta">Item 7</div>,
-  //   <div className="eshta">Item 8</div>
-  // ]
+  const items = [
+    <div className="eshta">Item 1</div>,
+    <div className="eshta">Item 2</div>,
+    <div className="eshta">Item 3</div>,
+    <div className="eshta">Item 4</div>,
+    <div className="eshta">Item 5</div>,
+    <div className="eshta">Item 6</div>,
+    <div className="eshta">Item 7</div>,
+    <div className="eshta">Item 8</div>,
+  ];
   return (
     <div>
       <Navbar />
       <Hero />
-      {/* <ItemsCarousel items={items}/> */}
+      {/* <ItemsCarousel items={items} /> */}
       {/* <h1>HomePage</h1> */}
     </div>
   );
